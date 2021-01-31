1db78ae5a64f8bfe980d943c71828d3de0a71eba
Erek Röös <erek.roeoes@outlook.de>
2021-01-31 21:44:41 +0100

    fix: typings were broken

--- types/config.ts
+++ types/config.ts
@@ -1,22 +1,22 @@
 import { Monitor } from 'redis-smq-monitor';
 
-export enum ConfigRedisDriver {
-    REDIS = "redis",
-    IOREDIS = "ioredis"
+export enum RedisDriver {
+    REDIS = 'redis',
+    IOREDIS = 'ioredis'
 }
 
 export interface ConfigInterface extends Monitor.ConfigInterface {
-    namespace?: string,
+    namespace?: string;
     redis: {
-        driver: ConfigRedisDriver,
+        driver: RedisDriver;
         options: {
-            [key:string]: any
-        }
-    },
+            [key: string]: any;
+        };
+    };
     log?: {
-        enabled: boolean,
+        enabled: boolean;
         options: {
-            [key:string]: any
-        }
-    },
+            [key: string]: any;
+        };
+    };
 }
